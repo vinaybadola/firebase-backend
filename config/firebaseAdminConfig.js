eed07678262da6ce15243efa81daa2aea06141cc
vinaybadola <vinaybadola.cs18@gmail.com>
2024-12-17 15:40:57 +0530

    fix :change configurationn

--- config/firebaseAdminConfig.js
+++ config/firebaseAdminConfig.js
@@ -8,9 +8,21 @@ Please replace `'./firebaseAdminConfig.json'` with the path to your Firebase ser
 3. Go to firebase console > project settings > service accounts > firebase admin sdk > generate the .json file 
 
 */
+require("dotenv").config();
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebaseAdminConfig.json");
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE || "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), 
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain : process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
