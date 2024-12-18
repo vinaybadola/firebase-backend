f2b095e0aa56a27acacd558d90059d99a7670697
vinaybadola <vinaybadola.cs18@gmail.com>
2024-12-18 19:57:57 +0530

    handle cors errors

--- server.js
+++ server.js
@@ -7,18 +7,17 @@ const authRoutes = require("./src/routes/authRoute");
 const connectDB = require("./config/database");
 const cors = require('cors');
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 connectDB();
 
-const corsOptions = {
-  origin: "https://full-stack-firebase-auth.vercel.app/", 
-  methods: 'GET,POST,PUT,DELETE', 
-};
-
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // Routes
 app.use("/api/auth", authRoutes);
